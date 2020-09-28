2fe5fdecd977d30c67d22615a2023b8baa5de4de
LeoYulinLi <LeoYulinLi@gmail.com>
2020-09-28 00:22:41 -0700

    Added local storage support

--- src/states/store.ts
+++ src/states/store.ts
@@ -2,5 +2,27 @@ import { createStore } from "redux";
 import { rootReducer, RootState } from "./rootReducer";
 
 export default function configureStore(preloadedState: RootState) {
-  return createStore(rootReducer, preloadedState)
+  const dataRaw = localStorage.getItem("storeData")
+  const store = createStoreBaseOnLocalStorage()
+
+  function createStoreBaseOnLocalStorage() {
+    if (dataRaw) {
+      const storeData = <RootState>JSON.parse(dataRaw)
+      const storeDateFixed = { ...storeData }
+      Object.values(storeData.expenses).forEach(it => {
+        storeDateFixed.expenses[it.id].date = new Date(it.date)
+      })
+      const storeVersion = localStorage.getItem("storeVersion")
+      const currentStoreVersion = "1"
+      localStorage.setItem("storeVersion", currentStoreVersion)
+      return createStore(rootReducer, (storeVersion === currentStoreVersion) ? storeDateFixed : preloadedState)
+    } else {
+      return createStore(rootReducer, preloadedState)
+    }
+  }
+
+  store.subscribe(() => {
+    localStorage.setItem("storeData", JSON.stringify(store.getState()))
+  });
+  return store
 }
